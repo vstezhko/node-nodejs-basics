f38f38bd0766a15001e44d14487014e729f618ac
viktoriastezko <viktoriastezhko@gmail.com>
2024-08-07 13:22:18 +0300

    feat: add create fn

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { access, constants } from 'node:fs/promises';
+import { writeFile } from 'node:fs/promises';
+
+const fileName = 'fresh.txt'
+const pathToCheck = `src/fs/files/${fileName}`;
+const data = 'I am fresh and young'
+
 const create = async () => {
-    // Write your code here 
+    try {
+        await access(pathToCheck, constants.R_OK | constants.W_OK);
+        throw new Error('FS operation failed');
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            await writeFile(pathToCheck, data);
+            console.log('File created successfully');
+        } else {
+            throw err;
+        }
+    }
 };
 
 await create();
