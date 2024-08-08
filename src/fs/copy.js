927d81c584ed9c9a46c308dc13d391d34a9c146d
viktoriastezko <viktoriastezhko@gmail.com>
2024-08-08 14:11:03 +0300

    feat: add copy fn

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,34 @@
+import { access, constants } from 'node:fs/promises';
+import { readdir, mkdir, appendFile, readFile } from 'node:fs/promises';
+
+
+const pathToCopy = `src/fs/files`;
+const pathToCreate = `src/fs/files_copy`;
+
 const copy = async () => {
-    // Write your code here 
+
+    try {
+
+        await access(pathToCopy, constants.R_OK | constants.W_OK);
+
+        try {
+            await access(pathToCreate, constants.F_OK);
+            throw new Error('FS operation failed');
+        } catch (err) {
+            if (err.code !== 'ENOENT') throw err; // Ошибка, если папка files_copy существует
+        }
+
+        await mkdir(pathToCreate);
+        const files = await readdir(pathToCopy);
+        files.forEach( async(file) => {
+            const contents = await readFile(`${pathToCopy}/${file}`, { encoding: 'utf8' });
+            await appendFile(`${pathToCreate}/${file}`, contents);
+        })
+        console.log('Files copied successfully');
+
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await copy();
